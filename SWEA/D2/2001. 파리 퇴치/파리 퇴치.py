d03e19ff55ba7b6dcf35692c3c45146f501eda58
Zerozero212 <2018111math@gmail.com>
2025-08-03 21:13:41 +0900

    [D2] Title: 파리 퇴치, Time: 68 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,17 +1,12 @@
 try_num = int(input())
 for trial in range(1,try_num+1):
     N, M = map(int,input().split())
-    arr = []
+    arr = [list(map(int, input().split())) for _ in range(N)]
     max = 0
-    for n in range(N):
-        arr.append(list(map(int,input().split())))
- 
+
     for idx_x in range(N-M+1):
         for idx_y in range(N-M+1):
-            temp = 0
-            for sub_idx_x in range(M):
-                for sub_idx_y in range(M):
-                    temp += arr[idx_x+sub_idx_x][idx_y+sub_idx_y]
-            if max < temp:
-                max = temp
+            temp = [arr[x][y] for x in range(idx_x, idx_x+M) for y in range(idx_y,idx_y+M)]
+            if max < sum(temp):
+                max = sum(temp)
     print(f'#{trial} {max}')

659395d4096a01cadc27001d0c0307deb27aa752
Zerozero212 <2018111math@gmail.com>
2025-08-05 16:22:11 +0900

    Update 정곤이의 단조 증가하는 수.py

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
@@ -1,4 +1,6 @@
+# incr : num이 단조증가하는 숫자인지 확인하는 함수
 def incr(num):
+    # num이 123이라면 [1, 2, 3]로 만드는 작업 123 > '123' > 1 2 3 > [1, 2, 3]
     num_arr = list(map(int,str(num)))
     for i in range(1,len(num_arr)):
         if num_arr[i-1] > num_arr[i]: return 0
@@ -7,6 +9,7 @@ def incr(num):
 def valid(arr1):
     max = -1
     for i in range(len(arr1)):
+        # i<j이기에 j는 시작점이 i+1
         for j in range(i+1,len(arr1)):
             temp = arr1[i] * arr1[j]
             if incr(temp):

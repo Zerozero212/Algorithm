a40fe9d61b52cdfe08e60f441dee75ecb39f69e8
Zerozero212 <2018111math@gmail.com>
2025-08-06 16:56:09 +0900

    [D4] Title: 정식이의 은행업무, Time: 66 ms, Memory: 59,264 KB -BaekjoonHub

--- SWEA/D4/4366. 정식이의 은행업무/정식이의 은행업무.py
+++ SWEA/D4/4366. 정식이의 은행업무/정식이의 은행업무.py
@@ -1,9 +1,3 @@
-def tr(num_list,num1):
-    t = 0
-    for i in range(len(num_list)):
-        t += num_list[i]*num1**(len(num_list)-i-1)
-    return t
-
 def valid(n_str,num):
     arr = []
     for i in range(len(n_str)):
@@ -11,7 +5,7 @@ def valid(n_str,num):
             if n_str[i] != idx: 
                 temp = n_str[:]
                 temp [i] = idx
-                arr.append(tr(temp,num))
+                arr.append(int(''.join(map(str,temp)),num))
     return set(arr)
 
 T = int(input())

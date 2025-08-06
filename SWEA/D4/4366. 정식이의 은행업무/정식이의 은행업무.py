521729140306a8b07410f9857d8e77d55055791a
Zerozero212 <2018111math@gmail.com>
2025-08-06 16:58:09 +0900

    Update 정식이의 은행업무.py

--- SWEA/D4/4366. 정식이의 은행업무/정식이의 은행업무.py
+++ SWEA/D4/4366. 정식이의 은행업무/정식이의 은행업무.py
@@ -2,10 +2,10 @@ def valid(n_str,num):
     arr = []
     for i in range(len(n_str)):
         for idx in range(num):
-            if n_str[i] != idx: 
+            if n_str[i] != idx: # 처음에 202에서 212만 고려하고, 222와 같이 2씩 바뀌는 것을 고려하지 못함
                 temp = n_str[:]
                 temp [i] = idx
-                arr.append(int(''.join(map(str,temp)),num))
+                arr.append(int(''.join(map(str,temp)),num)) # int(문자열, 숫자) > 문자열을 10진법으로 변경
     return set(arr)
 
 T = int(input())

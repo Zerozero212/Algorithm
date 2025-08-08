9ff76f641bcb9f23363db6049b827ae4469ec8d1
Zerozero212 <2018111math@gmail.com>
2025-08-08 09:18:00 +0900

    [D2] Title: 우주선착륙2, Time: 101 ms, Memory: 60,160 KB -BaekjoonHub

--- SWEA/D2/10760. 우주선착륙2/우주선착륙2.py
+++ SWEA/D2/10760. 우주선착륙2/우주선착륙2.py
@@ -1,18 +1,17 @@
+def cnt(arr1,n,m):
+    direct = [(0,-1),(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1)]
+    count = 0
+    for x in range(n):
+        for y in range(m):
+            temp = 0
+            for dx, dy in direct:
+                if 0 <= x+dx < n and 0 <= y+dy < m and arr1[x+dx][y+dy] < arr1[x][y]: temp += 1
+            if temp >= 4: count += 1
+    return count
+
 T = int(input())
-# directions을 활용해서 8개 구간 탐색
-for trial in range(1,T+1):
-    x,y = map(int,input().split())
-    array = [list(map(int,input().split())) for _ in range(x)]
 
-    directions = [(0,-1),(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1)]
-    result = 0
-
-    for idx_x in range(x):
-        for idx_y in range(y):
-            count = 0
-            for dx,dy in directions:
-                new_x = idx_x + dx
-                new_y = idx_y + dy
-                if 0 <= new_x < x and 0 <= new_y < y and array[idx_x][idx_y] > array[new_x][new_y]: count += 1
-            if count >= 4: result += 1
-    print(f'#{trial} {result}')
+for t in range(1,T+1):
+    N,M = map(int,input().split())
+    arr = [list(map(int,input().split())) for _ in range(N)]
+    print(f'#{t} {cnt(arr,N,M)}')

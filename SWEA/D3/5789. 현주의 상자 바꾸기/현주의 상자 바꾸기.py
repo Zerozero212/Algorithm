9ace0541c9fe4881b6d1d2eed10a841ecd0f4488
Zerozero212 <2018111math@gmail.com>
2025-08-10 15:27:55 +0900

    Update 현주의 상자 바꾸기.py

--- SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.py
+++ SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.py
@@ -1,9 +1,9 @@
 T = int(input())
 for tc in range(1,T+1):
     N,Q = map(int,input().split())
-    box = [0]*(N+1)
+    box = [0]*(N+1) #입력할 상자 생성
     for i in range(1,Q+1):
         L,R = map(int,input().split())
         for I in range(L,R+1):
-            box[I] = i
+            box[I] = i #해당 번호 박스에 바로 할당
     print(f'#{tc}',*box[1:])

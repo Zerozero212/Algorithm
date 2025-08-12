ffd6f364175929a28151de7380b6c017f6fe9c23
Zerozero212 <2018111math@gmail.com>
2025-08-12 22:18:58 +0900

    [Silver I] Title: 숨바꼭질, Time: 96 ms, Memory: 38148 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,20 +1,17 @@
 from collections import deque
-def bfs(x,t):
-    q = deque([(x,t)])
-    v[x]=1
-    while q:
-        nx,nt = q.popleft()
-        if nx == K: return nt
-        if nx-1 >= 0 and v[nx-1] == 0:
-            v[nx-1] = 1
-            q.append((nx-1,nt+1))
-        if nx+1 < 100001 and v[nx+1] == 0:
-            v[nx+1] = 1
-            q.append((nx+1,nt+1))
-        if 2*nx < 100001 and v[2*nx] == 0:
-            v[2*nx] = 1
-            q.append((2*nx,nt+1))
 
-N,K = map(int,input().split())
+def bfs():
+    q = deque([N])
+    v[N] = 1
+    while q:
+        x = q.popleft()
+        if x == K:
+            return v[x] - 1  # 시작을 1로 뒀으니 최종 시간은 -1
+        for nx in (x-1, x+1, x*2):
+            if 0 <= nx <= 100000 and v[nx] == 0:
+                v[nx] = v[x] + 1
+                q.append(nx)
+
+N, K = map(int, input().split())
 v = [0] * 100001
-print(bfs(N,0))
+print(bfs())

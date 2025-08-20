fa0caa2ae595dcfd7a3e8699812ca6524c0d4a77
Zerozero212 <2018111math@gmail.com>
2025-08-21 01:30:10 +0900

    [Silver II] Title: DFS와 BFS, Time: 140 ms, Memory: 115108 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -2,40 +2,45 @@ from collections import deque
 import sys
 input=sys.stdin.readline
 
-def DFS_with_adj_list(graph, root):
-    visited = []
-    stack = [root]
+def dfs(dfs_node):
+    if dfs_visited[dfs_node]: return
+    else:
+        dfs_visited[dfs_node] = 1
+        dfs_answer.append(dfs_node)
+        for linked_node in arr[dfs_node]:
+            dfs(linked_node)
 
-    while stack:
-        n = stack.pop()
-        if n not in visited:
-            visited.append(n)
-            stack += reversed(graph[n])
-    return visited
+def bfs(bfs_node):
+    q=deque([bfs_node])
+    bfs_visited[bfs_node]=1
+    while q:
+        new_node = q.popleft()
+        bfs_answer.append(new_node)
+        for linked_node in arr[new_node]:
+            if not bfs_visited[linked_node]: 
+                bfs_visited[linked_node]=1
+                q.append(linked_node)
 
-def BFS_with_adj_list(graph, root):
-    visited = []
-    queue = deque([root])
+# 입력
+N,M,V = map(int,input().split())
+arr = [[] for _ in range(N+1)]
 
-    while queue:
-        n = queue.popleft()
-        if n not in visited:
-            visited.append(n)
-            queue += graph[n]
-    return visited
+# 인접 리스트 생성
+for _ in range(M):
+    node1,node2 = map(int,input().split())
+    arr[node1].append(node2)
+    arr[node2].append(node1)
+arr = [sorted(x) for x in arr]
 
-node,edge,root_node=map(int,input().split())
-graph_list={i:[] for i in range(1,node+1)}
-for i in range(1,edge+1):
-    node1,node2=map(int,input().split())
-    graph_list[node1].append(node2)
-    graph_list[node2].append(node1)
+# dfs 준비
+dfs_visited = [0]*(N+1)
+dfs_answer = []
+dfs(V)
 
-for key in graph_list:
-    graph_list[key].sort()
+# bfs 준비
+bfs_visited = [0]*(N+1)
+bfs_answer = []
+bfs(V)
 
-for ch in DFS_with_adj_list(graph_list, root_node):
-    print(ch,end=' ')
-print()
-for ch in BFS_with_adj_list(graph_list, root_node):
-    print(ch,end=' ')
+print(*dfs_answer)
+print(*bfs_answer)

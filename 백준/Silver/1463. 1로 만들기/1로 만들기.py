f43a03d883f7f07a6f4962521f13ef511c0f1075
Zerozero212 <2018111math@gmail.com>
2025-09-07 21:00:59 +0900

    [Silver III] Title: 1로 만들기, Time: 460 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,12 +1,12 @@
-from collections import deque
-
 N=int(input())
-q=deque([(0,N)])
-while q:
-    cnt,nq=q.popleft()
-    if nq==1: break
-    if nq%3==0: q.append((cnt+1,nq//3))
-    if nq%2==0: q.append((cnt+1,nq//2))
-    q.append((cnt+1,nq-1))
+dp=[0]*(N+1)
 
-print(cnt)
+for i in range(2,N+1):
+    dp[i]=dp[i-1]+1
+
+    if i%2==0:
+        dp[i]=min(dp[i],dp[i//2]+1)
+    if i%3==0:
+        dp[i]=min(dp[i],dp[i//3]+1)
+
+print(dp[N])

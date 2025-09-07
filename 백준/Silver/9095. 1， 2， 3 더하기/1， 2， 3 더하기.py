e279ede0b7fee7eb309c39db959fe5eee02b6200
Zerozero212 <2018111math@gmail.com>
2025-09-07 18:20:10 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,17 +1,9 @@
-from collections import deque
-def sol():
-    ans=0
-    q=deque([(N,[])])
-    while q:
-        now,path=q.popleft()
-        if not now: ans+=1 
-        if now>0:
-            if now-1>=0: q.append((now-1,path+[1]))
-            if now-2>=0: q.append((now-2,path+[2]))
-            if now-3>=0: q.append((now-3,path+[3]))
-    return ans
+dp=[0]*12
+dp[1],dp[2],dp[3]=1,2,4
+for i in range(4,12):
+    dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
 
 T=int(input())
 for tc in range(1,T+1):
     N=int(input())
-    print(sol())
+    print(dp[N])

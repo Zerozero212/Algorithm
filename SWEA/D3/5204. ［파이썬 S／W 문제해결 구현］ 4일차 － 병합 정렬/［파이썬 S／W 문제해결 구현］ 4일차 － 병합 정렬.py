ca3a999324d7574051b047dbc4f72bb24c6ad2c1
Zerozero212 <2018111math@gmail.com>
2025-09-13 16:13:41 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 4일차 - 병합 정렬, Time: 1,519 ms, Memory: 181,056 KB -BaekjoonHub

--- SWEA/D3/5204. ［파이썬 S／W 문제해결 구현］ 4일차 － 병합 정렬/［파이썬 S／W 문제해결 구현］ 4일차 － 병합 정렬.py
+++ SWEA/D3/5204. ［파이썬 S／W 문제해결 구현］ 4일차 － 병합 정렬/［파이썬 S／W 문제해결 구현］ 4일차 － 병합 정렬.py
@@ -1,31 +1,30 @@
-from collections import deque
+def merge(li1,li2):
+    ans=[]
+    idx_1=idx_2=0
+    while idx_1<len(li1) and idx_2<len(li2):
+        if li1[idx_1]<li2[idx_2]: 
+            ans.append(li1[idx_1])
+            idx_1+=1
+        else: 
+            ans.append(li2[idx_2])
+            idx_2+=1
 
-def merge(li1, li2):
-    ans = []
-    while li1 and li2:
-        if li1[0] < li2[0]:
-            ans.append(li1.popleft())
-        else:
-            ans.append(li2.popleft())
-    if li1: ans.extend(li1)
-    if li2: ans.extend(li2)
+    if idx_1<len(li1): ans.extend(li1[idx_1:])
+    if idx_2<len(li2): ans.extend(li2[idx_2:])
     return ans
 
-def merge_sort(l, r):
+def merge_sort(li):
     global cnt
-    if l == r:
-        return [arr[l]]
-    mid = (l + r + 1) // 2        
-    left = merge_sort(l, mid-1)
-    right = merge_sort(mid, r)
-    if left[-1] > right[-1]:
-        cnt += 1
-    return merge(deque(left), deque(right))
+    if len(li)==1: return li
+    mid=len(li)//2
+    left=merge_sort(li[:mid])
+    right=merge_sort(li[mid:])
+    if left[-1]>right[-1]: cnt+=1
+    return merge(left,right)
 
-T = int(input())
-for tc in range(1, T+1):
-    N = int(input())
-    arr = list(map(int, input().split()))
-    cnt = 0
-    ans = merge_sort(0, N-1)
-    print(f'#{tc} {ans[N//2]} {cnt}')
+T=int(input())
+for tc in range(1,T+1):
+    N=int(input())
+    cnt=0
+    arr=merge_sort(list(map(int,input().split())))
+    print(f'#{tc} {arr[N//2]} {cnt}')

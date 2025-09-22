a48f1784c8d0270402c16cd3ef5726c566d6a111
Zerozero212 <2018111math@gmail.com>
2025-09-23 00:03:38 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 488 ms, Memory: 240576 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,9 +1,19 @@
-from collections import Counter
+import sys
+input = sys.stdin.readline
+
 N=int(input())
 arr=list(map(int,input().split()))
 M=int(input())
 search_num = list(map(int,input().split()))
 
-arr=Counter(arr)
+card=dict()
+for n in arr:
+    if n in card: card[n]+=1
+    else: card[n]=1
+
+result=[]
 for n in search_num:
-    print(arr[n],end=" ")
+    if n in card: result.append(card[n])
+    else: result.append(0)
+
+print(*result)

4b821fda8438354a06a7ad7d08e4531e54e13ec6
Zerozero212 <2018111math@gmail.com>
2025-09-23 01:22:49 +0900

    [Silver II] Title: 좌표 압축, Time: 984 ms, Memory: 284052 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,10 +1,9 @@
 import sys
 input = sys.stdin.readline
-from collections import Counter
 
 N=int(input())
 arr = list(map(int,input().split()))
-table = {v:i for i,v in enumerate(Counter(sorted(arr)).keys())}
+table = {v:i for i,v in enumerate(sorted(set(arr)))}
 
 for n in arr:
     print(table[n],end=" ")

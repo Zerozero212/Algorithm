a6644b0d0e7ed82865185f073b9970c15d5b6344
Zerozero212 <2018111math@gmail.com>
2025-10-07 14:55:59 +0900

    [Gold V] Title: 괄호의 값, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.py
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -1,46 +1,32 @@
-s = input().strip()
-stack = []
-
-for ch in s:
-    if ch in '([':
-        stack.append(ch)
-    elif ch == ')':
-        temp = 0
-        while stack and stack[-1] != '(':
-            if isinstance(stack[-1], int):
-                temp += stack.pop()
-            else:  # '['를 만나면 잘못된 괄호열
-                print(0)
-                exit()
-        if not stack:  # '('가 없으면 잘못된 괄호열
-            print(0)
-            exit()
-        stack.pop()  # '(' 제거
-        stack.append(2 if temp == 0 else 2 * temp)
-    elif ch == ']':
-        temp = 0
-        while stack and stack[-1] != '[':
-            if isinstance(stack[-1], int):
-                temp += stack.pop()
-            else:
-                print(0)
-                exit()
-        if not stack:
-            print(0)
-            exit()
-        stack.pop()  # '[' 제거
-        stack.append(3 if temp == 0 else 3 * temp)
+s=input()
+li=[]
+total=0
+tmp=1
+for i in range(len(s)):
+    if s[i] == "(":
+        li.append(s[i])
+        tmp*=2
+    elif s[i] == "[":
+        li.append(s[i])
+        tmp*=3
+    elif s[i] == ")":
+        if not li or li[-1] == "[":
+            total = 0
+            break
+        elif s[i-1] == "(":
+            total+=tmp
+        li.pop()
+        tmp//=2
     else:
-        print(0)
-        exit()
+        if not li or li[-1] == "(":
+            total=0
+            break
+        elif s[i-1] == "[":
+            total+=tmp
+        li.pop()
+        tmp//=3
 
-# 계산 끝나고 스택에 숫자만 남아야 함
-ans = 0
-for x in stack:
-    if isinstance(x, int):
-        ans += x
-    else:
-        print(0)
-        exit()
-
-print(ans)
+if li:
+    print(0)
+else:
+    print(total)

2f8a77e9f526e9020213720f271df30ca4ccf6c6
Zerozero212 <2018111math@gmail.com>
2025-10-11 21:42:18 +0900

    [Silver III] Title: 칸토어 집합, Time: 88 ms, Memory: 112164 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,19 +1,11 @@
 import sys
 
-def sol(n,total):
-    if n<0: return
-    
-    if n==0:
-        for i in range(1,total,2):
-            ans[i]=' '
-        return
-    
-    for i in range(3**n,total,2*3**n):
-        ans[i:i+3**n]=' '*3**n
-    sol(n-1,total)
+def cantor(n):
+    if n == 0:
+        return '-'
+    prev = cantor(n - 1)
+    return prev + ' ' * (3 ** (n - 1)) + prev
 
 for line in sys.stdin:
-    N=int(line.strip())
-    ans=['-']*3**N
-    sol(N-1,len(ans))
-    print(''.join(ans))
+    N = int(line.strip())
+    print(cantor(N))

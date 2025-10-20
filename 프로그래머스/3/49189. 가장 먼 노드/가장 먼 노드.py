870ee9a867e202d6ee1151308492737badfc9424
Zerozero212 <2018111math@gmail.com>
2025-10-20 23:06:37 +0900

    Update 가장 먼 노드.py

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,25 +1,21 @@
 from collections import deque
+
 def solution(n, edge):
-    graph = [[] for _ in range(n+1)]
-    for u,v in edge:
-        graph[u].append(v)
-        graph[v].append(u)
-    
-    def bfs():
-        q = deque([(0,1)])
-        v=[0]*(n+1)
-        cnt_list = [0]*(n+1)
-        
-        while q:
-            cnt,node = q.popleft()
-            if v[node]: continue
-            v[node]=1
-            cnt_list[node]=cnt
-            for nxt_node in graph[node]:
-                if v[nxt_node]: continue
-                q.append((cnt+1,nxt_node))
-        return cnt_list
-    
-    cnt_list = bfs()
-    
-    return cnt_list.count(max(cnt_list))
+    graph = [[] for _ in range(n + 1)]
+    for a, b in edge:
+        graph[a].append(b)
+        graph[b].append(a)
+
+    dist = [-1] * (n + 1)  # -1이면 아직 방문하지 않음
+    dist[1] = 0            # 1번 노드 시작점
+
+    q = deque([1])
+    while q:
+        node = q.popleft()
+        for nxt in graph[node]:
+            if dist[nxt] == -1:         # 아직 방문 안 했으면
+                dist[nxt] = dist[node] + 1
+                q.append(nxt)
+
+    max_dist = max(dist)
+    return dist.count(max_dist)

988d6d063771ac905ba4e3a2cbf5b8d0ca433cb2
Zerozero212 <2018111math@gmail.com>
2025-10-20 16:38:34 +0900

    [Gold V] Title: 1로 만들기 2, Time: 124 ms, Memory: 126368 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,27 +1,19 @@
-from heapq import heappush,heappop
-def sol():
-    pq = [(0,1)]
-    cnt=[1e9]*(N+1)
-    cnt[1]=0
-    v=[0]*(N+1)
+N = int(input())
+dp = [0]*(N+1)
+prev = [0]*(N+1)
 
-    while pq:
-        k,num = heappop(pq)
-        if v[num]: continue
-        if num==N: return k
-        v[num]=1
+for i in range(2, N+1):
+    dp[i] = dp[i-1] + 1
+    prev[i] = i-1
+    if i % 2 == 0 and dp[i] > dp[i//2] + 1:
+        dp[i] = dp[i//2] + 1
+        prev[i] = i//2
+    if i % 3 == 0 and dp[i] > dp[i//3] + 1:
+        dp[i] = dp[i//3] + 1
+        prev[i] = i//3
 
-        for nxt in (num+1,num*2,num*3):
-            if nxt<=N and k+1<cnt[nxt]:
-                cnt[nxt]=k+1
-                prev[nxt]=num
-                heappush(pq,(k+1,nxt))
-
-N=int(input())
-prev=[0]*(N+1)
-ans=sol()
-print(ans)
+print(dp[N])
 
 while N:
     print(N,end=' ')
-    N=prev[N]
+    N = prev[N]

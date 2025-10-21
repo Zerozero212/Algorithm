11164c5b1e8b6a0acd5751c6e8176b41da20cb21
Zerozero212 <2018111math@gmail.com>
2025-10-21 15:19:33 +0900

    [Silver III] Title: 퇴사, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,18 +1,12 @@
-N = int(input())
-T = [0]
-P = [0]
-dp = [0] * (N + 2)  
+N=int(input())
+consulting = [tuple(map(int,input().split())) for _ in range(N)]
+dp=[0]*(N+1)
 
-for _ in range(N):
-    t, p = map(int, input().split())
-    T.append(t)
-    P.append(p)
-
-for i in range(N, 0, -1):
-    tmp = i + T[i]
-    if tmp <= N + 1:
-        dp[i] = max(P[i] + dp[tmp], dp[i + 1])
+for i in range(N-1,-1,-1):
+    end_date = i+consulting[i][0]
+    if end_date<=N:
+        dp[i] = max(dp[end_date] + consulting[i][1],dp[i+1])
     else:
-        dp[i] = dp[i + 1]
+        dp[i] = dp[i+1]
 
-print(dp[1])
+print(dp[0])

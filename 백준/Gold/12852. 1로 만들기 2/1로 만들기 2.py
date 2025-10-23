4f756f35e2e0f8e34ecf096c8b01c717b56c209b
Zerozero212 <2018111math@gmail.com>
2025-10-23 20:43:13 +0900

    [Gold V] Title: 1로 만들기 2, Time: 124 ms, Memory: 126368 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,32 +1,3 @@
-## heap 자료구조를 활용한 풀이
-# from heapq import heappush,heappop
-# def sol():
-#     pq = [(0,1)]
-#     cnt=[1e9]*(N+1)
-#     cnt[1]=0
-#     v=[0]*(N+1)
-
-#     while pq:
-#         k,num = heappop(pq)
-#         if v[num]: continue
-#         if num==N: return k
-#         v[num]=1
-
-#         for nxt in (num+1,num*2,num*3):
-#             if nxt<=N and k+1<cnt[nxt]:
-#                 cnt[nxt]=k+1
-#                 prev[nxt]=num
-#                 heappush(pq,(k+1,nxt))
-
-# N=int(input())
-# prev=[0]*(N+1)
-# ans=sol()
-# print(ans)
-
-# while N:
-#     print(N,end=' ')
-#     N=prev[N]
-
 N = int(input())
 dp = [0]*(N+1)
 prev = [0]*(N+1)
@@ -45,5 +16,4 @@ print(dp[N])
 
 while N:
     print(N,end=' ')
-
     N = prev[N]

264d256d734434e9aba46f4da9bbfc756e1ea401
Zerozero212 <2018111math@gmail.com>
2025-10-29 21:19:20 +0900

    [level 2] Title: N-Queen, Time: 974.91 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -0,0 +1,21 @@
+def solution(n):
+    diag1=[0]*(2*n-1)
+    diag2=[0]*(2*n-1)
+    col=[0]*n
+    
+    ans=0
+    def dfs(row,cnt):
+        nonlocal ans
+        if cnt==n: 
+            ans+=1
+            return
+        if row>=n: return
+        
+        for i in range(n):
+            if col[i] or diag1[row+i] or diag2[i-row]: continue
+            col[i]=diag1[row+i]=diag2[i-row]=1
+            dfs(row+1,cnt+1)
+            col[i]=diag1[row+i]=diag2[i-row]=0
+    
+    dfs(0,0)
+    return ans

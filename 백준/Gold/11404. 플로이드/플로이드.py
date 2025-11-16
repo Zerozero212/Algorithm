855d46bfd8ec70525d7affc1a9e0e26d6d79d3fe
Zerozero212 <2018111math@gmail.com>
2025-11-16 15:04:10 +0900

    [Gold IV] Title: 플로이드, Time: 160 ms, Memory: 112072 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,23 +1,30 @@
+import sys
+input = sys.stdin.readline
+INF = 1e9
+
 N = int(input())
 M = int(input())
-costs=[[0]*N for _ in range(N)]
+
+costs = [[INF]*N for _ in range(N)]
 
 for _ in range(M):
-    u,v,w = map(int,input().split())
-    
-    if costs[u-1][v-1] == 0 or w < costs[u-1][v-1]:
-        costs[u-1][v-1] = w
+    u, v, w = map(int, input().split())
+    costs[u-1][v-1] = min(costs[u-1][v-1], w)
 
 for k in range(N):
     for i in range(N):
-        if k==i: continue
-        for j in range(N):
-            if i==j or k==j: continue
-            if 0 in (costs[i][k],costs[k][j]): continue
-            
-            tmp = costs[i][k]+costs[k][j]
-            if costs[i][j]==0 or tmp < costs[i][j]: 
-                costs[i][j] = tmp
 
-for row in costs:
-    print(*row)
+        if k==i: continue
+
+        for j in range(N):
+            if k==j or i==j: continue
+            if costs[i][j] > costs[i][k] + costs[k][j]:
+                costs[i][j] = costs[i][k] + costs[k][j]
+
+for i in range(N):
+    for j in range(N):
+        if costs[i][j] == INF:
+            print(0, end=' ')
+        else:
+            print(costs[i][j], end=' ')
+    print()
